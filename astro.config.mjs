921f37974966c4d41d86baca60a33d689d5d7c62
Ashley Rodan <ashleyrodan@gmail.com>
2024-11-08 21:18:21 +1100

    social shares and billies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://ashley.rodan.co/', // Replace with your actual live site URL
   integrations: [tailwind()],
 });
